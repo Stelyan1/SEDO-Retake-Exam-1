8a0929eb1ef7ce3eb221838c0481c7817a2b2627
Stelyan1 <stilqnbliznakov@gmail.com>
2025-12-16 10:58:29 +0200

    fixed home-load (#5)

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-document.getElementById('apply-btn').addEventListener('click', function () {
-  var c = document.getElementById('color-input').value;
-  document.getElementById('box').style.backgroundColor = c;
+document.getElementById('apply-btn').addEventListener('click', function() {
+  var color = document.getElementById('color-input').value;
+  document.getElementById('box').style.backgroundColor = color;
 });
 
 // Helper to set box color
